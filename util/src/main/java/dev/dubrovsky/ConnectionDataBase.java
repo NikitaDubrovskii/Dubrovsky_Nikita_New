48d2d865d054c028b520340ded0661e8b1d2ba6c
NikitaDubrovskii <inikita.dubrovskii@gmail.com>
2024-07-03 15:39:18 +0300

    Создан модуль Util.

---
+++ util/src/main/java/dev/dubrovsky/ConnectionDataBase.java
@@ -0,0 +1,34 @@
+package dev.dubrovsky;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class ConnectionDataBase {
+
+    private static final Properties properties = new Properties();
+    private static Connection connection;
+
+    private ConnectionDataBase() {
+
+    }
+
+    public static Connection getConnection() {
+        try (FileInputStream inputStream = new FileInputStream("util/src/main/resources/util.properties")) {
+            if (connection == null) {
+                properties.load(inputStream);
+                connection = DriverManager.getConnection(
+                        properties.getProperty("db.url"),
+                        properties.getProperty("db.user"),
+                        properties.getProperty("db.password")
+                );
+            }
+
+            return connection;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
