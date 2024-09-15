ba4b89d59a7c560048a96915b14d50757c1a1cfd
NikitaDubrovskii <inikita.dubrovskii@gmail.com>
2024-09-16 00:31:12 +0300

    Добавлено новое исключение. Добавлен обработчик исключений.

---
+++ exception/src/main/java/dev/dubrovsky/exception/handler/RestExceptionHandler.java
@@ -0,0 +1,43 @@
+package dev.dubrovsky.exception.handler;
+
+import dev.dubrovsky.exception.EntityNotFoundException;
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@ControllerAdvice
+@AllArgsConstructor
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleInternalDboException(EntityNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(),
+                HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Object> handleInternalDboException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(ex.getMessage(),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(@Nonnull Exception ex,
+                                                             @Nullable Object body,
+                                                             @Nonnull HttpHeaders headers,
+                                                             @Nonnull HttpStatusCode statusCode,
+                                                             @Nonnull WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
--- util/src/main/java/dev/dubrovsky/util/validation/ValidationUtil.java
+++ util/src/main/java/dev/dubrovsky/util/validation/ValidationUtil.java
@@ -1,31 +1,12 @@
 package dev.dubrovsky.util.validation;
 
+import dev.dubrovsky.exception.EntityNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.NoSuchElementException;
 
 public final class ValidationUtil {
 
     private ValidationUtil() {
     }
-
-    /*public static <T> void checkId(Integer id, AbstractDao<T> dao) {
-        check(id, dao);
-    }
-
-    public static <T> void checkEntityPresent(Integer id, AbstractDao<T> dao) {
-        check(id, dao);
-    }*/
-
-    /*private static <T> void check(Integer id, AbstractDao<T> dao) {
-        if (id > 0) {
-            Optional
-                    .ofNullable(dao.getById(id))
-                    .orElseThrow(() -> new NoSuchElementException("Ничего не найдено с id: " + id));
-        } else {
-            throw new IllegalArgumentException("Id должен быть больше 0");
-        }
-    }*/
 
     public static <T> void checkId(Integer id, JpaRepository<T, Integer> repository) {
         check(id, repository);
@@ -36,10 +17,10 @@ public final class ValidationUtil {
     }
 
     private static <T> void check(Integer id, JpaRepository<T, Integer> repository) {
-        if (id > 0) {
+        if (id >= 0) {
             repository
                     .findById(id)
-                    .orElseThrow(() -> new NoSuchElementException("Ничего не найдено с id: " + id));
+                    .orElseThrow(() -> new EntityNotFoundException("Ничего не найдено с id: " + id));
         } else {
             throw new IllegalArgumentException("Id должен быть больше 0");
         }
