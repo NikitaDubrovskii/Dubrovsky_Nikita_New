948554f9622575ffacb9b70f454fb13e795e1d75
NikitaDubrovskii <inikita.dubrovskii@gmail.com>
2024-10-20 23:08:35 +0300

    Обработана ситуация, когда истекает токен (сообщение пользователю).

--- config/src/main/java/dev/dubrovsky/config/JWTFilter.java
+++ config/src/main/java/dev/dubrovsky/config/JWTFilter.java
@@ -1,8 +1,8 @@
 package dev.dubrovsky.config;
 
-import dev.dubrovsky.model.user.User;
 import dev.dubrovsky.service.user.UserDetailsServiceImpl;
 import dev.dubrovsky.util.jwt.JWTTokenUtil;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -39,12 +39,18 @@ public class JWTFilter extends OncePerRequestFilter {
 
         if (authorizationHeader != null && authorizationHeader.startsWith(BEARER_PREFIX)) {
             jwt = authorizationHeader.substring(BEARER_PREFIX.length());
-            username = jwtTokenUtil.extractUserName(jwt);
+            try {
+                username = jwtTokenUtil.extractUserName(jwt);
+            } catch (ExpiredJwtException e) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("Token has expired, please refresh it.");
+                return;
+            }
         }
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = userService.loadUserByUsername(username);
-            if (jwtTokenUtil.validateToken(jwt, (User) userDetails)) {
+            if (jwtTokenUtil.validateToken(jwt, userDetails)) {
                 UsernamePasswordAuthenticationToken authToken =
                         new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
